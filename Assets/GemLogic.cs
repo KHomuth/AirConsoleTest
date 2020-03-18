d4b88828665fafc2b7fff5f17ea9eccff38165b8
Kevin Homuth <kevin.homuth@haw-hamburg.de>
2020-03-18 17:11:50 +0100

    added basic airconsole

---
+++ Assets/GemLogic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NDream.AirConsole;
+using Newtonsoft.Json.Ling;
+
+public class GemLogic : MonoBehaviour
+{
+    void Awake ()
+	{
+        AirConsole.instance.onMessage += OnMessage;
+	}
+
+    void OnMessage (int fromDeviceID, JToken data)
+	{
+        Debug.Log("message from" + fromDeviceID + ", data: " + data);
+        if (data ["action"] != null && data ["action"].ToString ().Equals ("interact"))
+		{
+            Camera.main.backgroundColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+		}
+	}
+
+    void OnDestroy ()
+	{
+        if (AirConsole.instance != null)
+		{
+			AirConsole.instance.ofMessage -= OnMessage;
+		}
+	}
+}
